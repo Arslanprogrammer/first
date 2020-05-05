d9eff0e0dcb2e706b3f37d4b9603f903c802bbc4
Muhammad Arslan <arslanabdulrazzaq@outlook.com>
2020-05-05 20:11:28 +0500

    implemented css

--- header.php
+++ header.php
@@ -1,4 +1,10 @@
-<h1>header</h1>
+<!DOCTYPE HTML>
+
+<head>
+    <title>Muhammad Arslan</title>
+    <link rel="stylesheet" href="app.css" />
+</head>
+<header><h1> logo</h1></header>
 <ul>
     <li><a href="index.php">Home</a></li>
     <li><a href="about.php">about</a></li>
